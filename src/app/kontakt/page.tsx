1707cb5ea360d96929f602a27bfab0f6dfce123a
Julius Laux <juliuslaux@gmail.com>
2025-05-16 09:29:46 +0200

    made input fields required for contactr form

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -279,6 +279,7 @@ export default function Contact() {
                       onChange={handleChange}
                       placeholder="Dr. Melanie Schmidt"
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#1A371C] sm:text-sm sm:leading-6 font-[Instrument_Sans]"
+                      required
                     />
                   </div>
                 </div>
@@ -296,6 +297,7 @@ export default function Contact() {
                       onChange={handleChange}
                       placeholder="info@tierklinik-schmidt.de"
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#1A371C] sm:text-sm sm:leading-6 font-[Instrument_Sans]"
+                      required
                     />
                   </div>
                 </div>
@@ -313,6 +315,7 @@ export default function Contact() {
                       onChange={handleChange}
                       placeholder="+49 151 1234 5678"
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#1A371C] sm:text-sm sm:leading-6 font-[Instrument_Sans]"
+                      required
                     />
                   </div>
                 </div>
@@ -329,6 +332,7 @@ export default function Contact() {
                       onChange={handleChange}
                       placeholder="Tierklinik Dr. Schmidt GmbH"
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#1A371C] sm:text-sm sm:leading-6 font-[Instrument_Sans]"
+                      required
                     />
                   </div>
                 </div>
@@ -345,6 +349,7 @@ export default function Contact() {
                       value={formData.website}
                       onChange={handleChange}
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#1A371C] sm:text-sm sm:leading-6 font-[Instrument_Sans]"
+                      required
                     />
                   </div>
                 </div>
